b055ab69c250a76c342571e273549aebb49d8750
AlanAntony123 <alanvadakkumpadan@gmail.com>
2023-04-13 23:08:31 -0700

    add

---
+++ schoolstore/schoolproject/school/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+
+from school.models import depart
+
+# Register your models here.
+admin.site.register(depart)
---
+++ schoolstore/schoolproject/school/views.py
@@ -0,0 +1,63 @@
+from django.contrib import auth, messages
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
+from .models import depart
+
+
+# Create your views here.
+def demo(request):
+    obj = depart.objects.all()
+    return render(request, 'index.html', {'result': obj})
+
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth.authenticate(username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/')
+        else:
+            messages.info(request, 'invalid credentials')
+            return redirect('login')
+    return render(request, 'login.html')
+
+
+
+
+
+def register(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        email = request.POST['email']
+        password = request.POST['password']
+        password1 = request.POST['password1']
+        if password == password1:
+            if User.objects.filter(username=username).exists():
+                messages.info(request, 'username taken ')
+                return redirect('register')
+            elif User.objects.filter(email=email).exists():
+                messages.info(request, 'email taken ')
+                return redirect('register')
+            else:
+                user = User.objects.create_user(username=username, first_name=first_name, last_name=last_name,
+                                                password=password, email=email)
+                user.save();
+            return redirect('login')
+
+        else:
+            messages.info(request, 'password not matching')
+            return redirect('register')
+        return redirect('/')
+    return render(request, 'register.html')
+
+
+def form(request):
+    return render(request, 'form.html')
+
+
+def logout(request):
+    return redirect('/school')
